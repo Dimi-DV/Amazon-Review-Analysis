ac391a41ddfcab5fb0dd8c6693efa3ff17a0411d
Dimitrije Davidovic Vedda <114122475+Dimi-DV@users.noreply.github.com>
2023-08-10 16:00:01 -0400

    Update review_scraper.py

--- scripts/review_scraper.py
+++ scripts/review_scraper.py
@@ -1,3 +1,12 @@
+'''
+Web Scraping Disclaimer
+
+This code is provided for educational and informational purposes only. Web scraping may be against 
+the terms of service of some websites. Amazon's terms of service, in particular, prohibit scraping. 
+Use this code at your own risk. The developer is not responsible for any legal or other consequences 
+that may result from its use.
+'''
+
 import json
 import time
 import random
